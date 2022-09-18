1dba1ba552bf4ff8ffb98e21ca1117cda251ad7e
Lee <errestan@users.noreply.github.com>
2022-09-18 11:28:41 +0100

    NeoVim: Remove ENTER as the completion key
    Disable mapping the enter key to the completion accept command as this
    was causing problems.

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -20,10 +20,10 @@ config = {
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        }),
+        -- ['<CR>'] = cmp.mapping.confirm({
+        --     behavior = cmp.ConfirmBehavior.Replace,
+        --     select = true,
+        -- }),
         ['<Tab>'] = cmp.mapping(
             function(fallback)
                 if cmp.visible() then
