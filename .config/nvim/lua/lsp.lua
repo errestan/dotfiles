6b231d0cd14c3ad734b918ea8b97a7235bf90367
Lee <errestan@users.noreply.github.com>
2022-12-12 11:48:38 +0000

    [NeoVim] Update LSP configuration
    Fixed use of deprecated function when setting up LSP integration with
    completion plug-in.

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -32,7 +32,7 @@ end
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Use a loop to conveniently both setup defined servers 
 -- and map buffer local keybindings when the language server attaches
